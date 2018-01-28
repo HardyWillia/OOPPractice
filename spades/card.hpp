a4353510bd897f4e118b7813248003ec2b987d94
Willia Hardy <hardywillia@outlook.com>
2018-01-28 16:11:03 -0500

    Added main and card header program. Added build directory

---
+++ spades/card.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+enum Rank {
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+	Ace,
+	Deuce,
+	Little Joker,
+	Big Joker
+};
+
+
+enum Suit {
+	Spade,
+	Diamond,
+	Heart,
+	Club
+};
--- spades/main.cpp
+++ spades/main.cpp
@@ -1,1 +1,10 @@
 //New file for game of spades
+
+#include "card.hpp"
+#include <iostream>
+
+
+int main() {
+	std::cout << "Demo for the card game of spades\n";
+
+}
