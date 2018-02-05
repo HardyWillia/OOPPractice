1aa6d85b0d66195dd1db00fdcf743584a3bcf141
Willia Hardy <hardywillia@outlook.com>
2018-02-04 19:14:21 -0500

    Implemented string class for copy, compare, and default

--- string/string.cpp
+++ string/string.cpp
@@ -4,21 +4,30 @@
 
 int main(){
 
+	//Default construct
+	string s0;
+
+	//Construct and assign from a literal value
+	string s1 = "hello";
+	s1 = "foo";
+
+	//Copy construct and assign
+	string s2 = s1;
+	s2 = s1;
+
+	//Compare  
+	s1 = s1;
+
+	//Order
+	s1 <s1;
+
+	//Object independence
+	s2 = "bar";
 
 
 
 
 
+	return 0;
 
-
-
-
-
-
-
-
-
-
-
-return 0;
 }
--- string/string.hpp
+++ string/string.hpp
@@ -1,1 +1,31 @@
 //Header file for string class
+
+struct String {
+
+	int len;
+	char *s;
+	
+	//Default constructor 
+	String():len(),str(nullptr){}
+
+	//Copy constructor
+	String(char const *s):len(strlen(s)), str(new char[len+1]){
+		strcpy(str, s);
+
+	}
+
+	//Destructor - dtor
+	~String(){
+		delete[]str;
+	}
+
+	String & operator=(String const &s){
+
+		delete[]str;
+		len = s.len;
+		str = new char[len+1];
+		strcpy(str, s.str);
+		return *this;
+	}
+}	
+
