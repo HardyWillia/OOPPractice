9d6c61e60ae10c221304a51c3f95678c556ae4a4
Willia Hardy <wjh26@zips.uakron.edu>
2018-02-12 12:40:43 -0500

    Implemented equality, ordering, and default constructor in card class for bitfields

--- spades/card.hpp
+++ spades/card.hpp
@@ -1,3 +1,7 @@
+#pragma once
+
+#include <iostream>
+
 enum Rank {
 	Two, //Special case since using the jokers. Is not used for Hearts and Diamonds
 	Three,
@@ -27,10 +31,48 @@ enum Suit {
 
 
 struct Card{
+public:
+
+	//Changed struct to implement bitfields for Practice 
+
+	//Default constructor
+	Card(){}
 
 	Card(Rank r,  Suit s)
-	: rank(r), suit(s) {}
+	: bits((unsigned)s << 4 | (unsigned)r) {}
+    
+	Rank get_rank() const {
+		return(Rank)(0b001111 & bits);
+	}
 
+	Suit get_suit() const {
+		return(Suit)((0b110000 & bits) >> 4); //have to shift back to keep unsigned format
+	}
+  
+	//Copyable
+		
 
+	//Equality
+	bool operator==(Card c) const {
+		return bits == c.bits;
+	}
+
+	bool operator!=(Card c) const{
+		return bits != c.bits;
+	}
+
+	//Ordering
+	bool operator <=(Card c) const {
+		return bits <= c.bits;
+	}
+
+	bool operator >=(Card c) const {
+		return bits >= c.bits;
+	}
+
+	//Destructor
+
+private:
+	unsigned char bits;
 };
 
