0340be5c8ff137fcb22a60f8150d9196fb326b3c
Willia Hardy <wjh26@zips.uakron.edu>
2018-03-19 19:44:39 -0400

    Separated player information from introduction class for later enhancements

--- santaparavia/intro.hpp
+++ santaparavia/intro.hpp
@@ -1,8 +1,6 @@
 /*Changes introductory information and functions of the game
 	* Initial instructions that are printed to the screen (PrintIntructions) 
 		* Make these mandatory maybe?
-	* Player name and information
-*/
 
 
 struct Introduction {
---
+++ santaparavia/player.hpp
@@ -0,0 +1,14 @@
+/*Player information and initial characteristics
+ ***Separated this function from intro.hpp to include more player characteristics later
+*/
+
+
+
+struct Player {
+
+	Player(){}
+	~Player(){}
+
+
+
+};
