cca914047c44c5936dfc8058523cebbf30da91f7
Willia Hardy <wjh26@zips.uakron.edu>
2018-03-19 19:40:38 -0400

    Created header file and decided what specifically to change about the game for part 2 of project

---
+++ santaparavia/intro.hpp
@@ -0,0 +1,14 @@
+/*Changes introductory information and functions of the game
+	* Initial instructions that are printed to the screen (PrintIntructions) 
+		* Make these mandatory maybe?
+	* Player name and information
+*/
+
+
+struct Introduction {
+	
+	Intro() {}
+	~Intro() {}
+
+
+};
---
+++ santaparavia/paravia.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(){
+
+	std::cout << "Start of game enhancements\n";
+	
+
+
+	return 0;
+}
