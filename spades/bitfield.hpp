effec33d5abfec4c4e179464d40d760091e787e0
Willia Hardy <wjh26@zips.uakron.edu>
2018-04-03 21:43:09 -0400

    Added bitfield implementation

--- spades/bitfield.hpp
+++ spades/bitfield.hpp
@@ -1,1 +1,63 @@
 //Start of bitfield card class
+
+#include <cassert>
+#include <iosfwd>
+
+enum Rank {
+
+	Ace,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+};
+
+
+enum Suit {
+	Spades,
+	Clubs,
+	Diamonds,
+	Hearts,
+};
+
+enum Color {
+	Black, 
+	Red,
+};
+
+class Card{
+public:
+
+	//Default constructor
+	Card() = default;
+
+	Card(Rank r, Suit s, Color c)
+	: bits((unsigned)s << 4 | (unsigned)r | (unsigned)c){}
+
+	Rank get_rank() const {
+		return(Rank)(0b001111 & bits);
+	}
+
+	Suit get_suit() const {
+		return(Suit)((0b110000 & bits) >> 4);
+	}
+
+	Color get_color() const {
+	//	return(Color)(0b000000 & bits);
+	}
+
+private:
+	unsigned char bits;
+
+
+
+};
+
--- spades/unioncard.hpp
+++ spades/unioncard.hpp
@@ -1,1 +1,38 @@
 //Start of discriminated union-based card class
+//Start of object-oriented design card class using an inheritance hierarchy
+//Start of bitfield card class
+
+#include <cassert>
+#include <iosfwd>
+
+enum Rank {
+
+	Ace,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+};
+
+
+enum Suit {
+	Spades,
+	Clubs,
+	Diamonds,
+	Hearts,
+};
+
+enum Color {
+	Black, 
+	Red,
+};
+
+
