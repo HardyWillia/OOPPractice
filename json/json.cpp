11ade21a821e1d74c10fedf9757774898c872e20
Willia Hardy <wjh26@zips.uakron.edu>
2018-04-11 02:21:05 -0400

    Fixed commented fields from skeleton

--- json/json.cpp
+++ json/json.cpp
@@ -6,10 +6,13 @@
 
 
 
-/// Returns true if at the end of the file. bool is_eof(const char* first, const char* last) {
+/// Returns true if at the end of the file. 
+bool is_eof(const char* first, const char* last) {
   return first == last;
 }
-/// Returns true if *first is a punctuation character. bool is_punctuation(const char* first, const char* last) 
+
+/// Returns true if *first is a punctuation character. 
+bool is_punctuation(const char* first, const char* last) 
 {
   if (is_eof(first, last))
     return false;
@@ -25,15 +28,20 @@
     return true;
   }
 }
-/// Returns true if *first is a space character. bool is_space(const char* first, const char* last) {
+
+/// Returns true if *first is a space character. 
+bool is_space(const char* first, const char* last) {
   if (is_eof(first, last))
     return false;
   return std::isspace(*first);
 }
-/// Returns true if first delimits two words. bool is_delimiter(const char* first, const char* last) {
+
+/// Returns true if first delimits two words. 
+bool is_delimiter(const char* first, const char* last) {
   return is_eof(first, last)
       || is_space(first, last) is_punctuation(first, last);
 }
+
 bool match_literal(const char*& first, const char* last, const char* word) {
   auto q = word;
   while (!is_eof(first, last) && *q != '\0' && *first == *q) {
@@ -46,10 +54,13 @@ bool match_literal(const char*& first, const char* last, const char* word) {
     throw std::runtime_error("invalid literal");
   return true;
 }
-/// Skip whitespace. void skip_space(const char*& first, const char* last) {
+
+/// Skip whitespace. 
+void skip_space(const char*& first, const char* last) {
   while (is_space(first, last))
     ++first;
 }
+
 /// A helper class that can be used to skip whitespace around a term. /// /// This is called a guard or sentinel 
 class. It executes code *around* /// a region of code. Also called an RAII helper. /// /// RAII -- Resource 
 Acquisition Is Initialization struct space_guard {
@@ -65,20 +76,28 @@ Acquisition Is Initialization struct space_guard {
   const char*& first;
   const char* last;
 };
-/// Parse a value from a range of characters. Value* parse_value(const char*& first, const char* last); /// 
+
+/// Parse a value from a range of characters. 
+Value* parse_value(const char*& first, const char* last); /// 
 Parse the true value. Bool* parse_true(const char*& first, const char* last) {
   match_literal(first, last, "true");
   return new Bool(true);
 }
-/// Parse the false value. Bool* parse_false(const char*& first, const char* last) {
+
+/// Parse the false value. 
+Bool* parse_false(const char*& first, const char* last) {
   match_literal(first, last, "false");
   return new Bool(false);
 }
-/// Parse the null value. Null* parse_null(const char*& first, const char* last) {
+
+/// Parse the null value. 
+Null* parse_null(const char*& first, const char* last) {
   match_literal(first, last, "null");
   return new Null();
 }
-/// Parse a string value. String* parse_string(const char*& first, const char* last) {
+
+/// Parse a string value. 
+String* parse_string(const char*& first, const char* last) {
   const char* start = first++;
   while (!is_eof(first, last) && *first != '"') {
     if (*first == '\\')
@@ -89,7 +108,9 @@ Parse the true value. Bool* parse_true(const char*& first, const char* last) {
   ++first;
   return new String(std::string(start, first));
 }
-/// Parse a numeric value. Number* parse_number(const char*& first, const char* last) {
+
+/// Parse a numeric value. 
+Number* parse_number(const char*& first, const char* last) {
   const char* start = first++;
   while (!is_eof(first, last) && !is_delimiter(first, last))
     ++first;
@@ -102,7 +123,9 @@ Parse the true value. Bool* parse_true(const char*& first, const char* last) {
     throw std::runtime_error("invalid number");
   return new Number(d);
 }
-/// Parse an array. Array* parse_array(const char*& first, const char* last) {
+
+/// Parse an array. 
+Array* parse_array(const char*& first, const char* last) {
   // Pre-allocate the array.
   Array* arr = new Array();
   ++first;
@@ -130,12 +153,14 @@ Parse the true value. Bool* parse_true(const char*& first, const char* last) {
   ++first;
   return arr;
 }
-/// Parse a key. This simply parses a string, ensuring that surrounding /// spaces are skipped. String* 
-parse_key(const char*& first, const char* last) {
+/// Parse a key. This simply parses a string, ensuring that surrounding /// spaces are skipped. 
+String* parse_key(const char*& first, const char* last) {
   space_guard g(first, last);
   return parse_string(first, last);
 }
-/// Parse an object. Object* parse_object(const char*& first, const char* last) {
+
+/// Parse an object. 
+Object* parse_object(const char*& first, const char* last) {
   // Pre-allocate the objet.
   Object* obj = new Object();
   ++first;
@@ -169,7 +194,9 @@ parse_key(const char*& first, const char* last) {
   ++first;
   return obj;
 }
-/// The value parsed depends on the current character. Value* parse_value(const char*& first, const char* last) 
+
+/// The value parsed depends on the current character. 
+Value* parse_value(const char*& first, const char* last) 
 {
   if (is_eof(first, last))
     return nullptr;
