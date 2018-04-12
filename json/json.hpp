6bb0ee965f86ea120e9af77baa068caa217cfd9a
Willia Hardy <wjh26@zips.uakron.edu>
2018-04-11 22:07:08 -0400

    Added virtual overrides to JClass

--- json/json.hpp
+++ json/json.hpp
@@ -43,7 +43,32 @@ public:
 	bool is_array() const { return type() = Array; }
 	bool is_object() const { return type() = Object; }
 	
+	double number_value() const;
+	int int_value() const;
+	bool bool_value() const;
+	const std::string &string_value() const;
+	const array &array_items() const;
+	const object &object_items() const;
+
+	//Return a reference to the key of the object
+	const JClass & operator[](const std::string &key) const;
+
+
+	
 };
 
+class JClassValue {
+
+	virtual JClass::Type type() const = 0;
+	virtual bool bool_value() const;
+	virtual double number_value() const;
+	virtual int int_value() const;
+	virtual const std::string &string_value() const;
+	virtual const JClass::array &array_items() const;
+	virtual const JClass &operator[](const std::string &key) const;
+
+	virtual ~JClassValue(){}
+
+};
 	
 
