8490639c818e8190a8a90de859d81937d176a4fb
Willia Hardy <wjh26@zips.uakron.edu>
2018-04-11 21:59:43 -0400

    Added type definitions and enum

--- json/json.hpp
+++ json/json.hpp
@@ -4,10 +4,46 @@
 #include <iomanip>
 #include <map>
 #include <vector>
+#include <string>
+
+
+enum Type {
+
+	None,
+	Number,
+	Bool,
+	String,
+	Array,
+	Object
+};
+
 
 struct JClass {
+public:
+	
+	std::vector<JClass> array;
+	std::map<std::string, JClass> object;
 
-	virtual ~JClass() = default;
+	JClass(int value);
+	JClass(double value);
+	JClass(bool value);
+	JClass(const std::string &value);
+	JClass(const char *value);
+	JClass(const array &values);
+	JClass(const object &values);
 
+	JClass(void *) = delete;
+
+	Type type() const;
+	
+	bool is_none() const { return type() = None; }
+	bool is_number() const { return type() = Number; }
+	bool is_bool() const { return type() = Bool; }
+	bool is_string() const { return type() = String; }
+	bool is_array() const { return type() = Array; }
+	bool is_object() const { return type() = Object; }
+	
 };
+
+	
 
