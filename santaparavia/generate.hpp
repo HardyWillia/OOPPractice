3b130818a8a6e2441daee05d4e7db58cc578601a
Willia Hardy <wjh26@zips.uakron.edu>
2018-05-01 23:13:41 -0400

    Added to generate income class

--- santaparavia/generate.hpp
+++ santaparavia/generate.hpp
@@ -74,11 +74,50 @@ struct GenIncome : public Generate {
 	int nobles = 4;
 	int clergy = 5;
 	int merchants = 25;
+	char just[256];
 
 	void income(){
 		float y;
 		int revenues = 0;
+
+		JusticeRevenue = (Justice * 300 - 500) * TitleNum;
+		std::cin >> just;
+
+		//Type of justice determined by the user
+		switch(Justice){
+			case 1: strcpy(just, "Very Fair"); break;
+			case 2: strcpy(just, "Moderate"); break;
+			case 3: strcpy(just, "Harsh"); break;
+			case 4: strcpy(just, "Outrageous");
+		}
 		
+		y = 150.0 - (float)SalesTax - (float)CustomsDuty - (float)IncomeTax;
+		if( y < 1.0)
+			y = 1.0;
+		y /= 100.0;
+
+		//Generate Revenue
+		CustomsDutyRevenue = (nobles * 100) + (clergy * 75) + (merchants * 20 * y);
+		CustomsDutyRevenue += (int)(PublicWorks * 100.0);
+		CustomsDutyRevenue = (int)((float)(CustomsDuty/100) * (float)(CustomsDutyRevenue));
+
+		SalesTaxRevenue = (nobles * 50) + (merchants * 25) + (int)(PublicWorks * 10.0);
+		SalesTaxRevenue *= (y * (5 - Justice) * SalesTax);
+		SalesTaxRevenue /= 200;
+	
+		IncomeTaxRevenue = (nobles * 250) + (int)(PublicWorks * 20.0);
+		IncomeTaxRevenue += (10* Justice * nobles * y);
+		IncomeTaxRevenue *= IncomeTax;
+		IncomeTaxRevenue /= 100;
+		
+		
+		revenues = CustomsDutyRevenue + SalesTaxRevenue + IncomeTaxRevenue + JusticeRevenue;
+
+		std::cout << "State revenues " << revenues << " gold florins.\n";
+		std::cout << "Customs Duty \t Sales Tax \t Income Tax \t Justice";
+		std::cout << CustomsDutyRevenue << "\t\t" << SalesTaxRevenue << "\t\t" << IncomeTaxRevenue << "\t\t" << JusticeRevenue << "\t\t" << just << "\n";
+
+		return;
 
 	}
 
